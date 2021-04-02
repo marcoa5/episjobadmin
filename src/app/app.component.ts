81342ee8758002c584f1bc0a939142d3d063e3e7
marcoa5 <marco.arato@gmail.com>
2021-04-02 16:10:03 +0200

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HostListener } from '@angular/core'
+import * as firebase from 'firebase/app'
+import 'firebase/database'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBtO5C1bOO70EL0IPPO-BDjJ40Kb03erj4",
+  authDomain: "epi-serv-job.firebaseapp.com",
+  databaseURL: "https://epi-serv-job-default-rtdb.firebaseio.com",
+  projectId: "epi-serv-job",
+  storageBucket: "epi-serv-job.appspot.com",
+  messagingSenderId: "793133030101",
+  appId: "1:793133030101:web:a79f477c42cb9e0a53a05c",
+  measurementId: "G-C2CPM0MBH8"
+};
 
 @Component({
   selector: 'app-root',
@@ -10,6 +23,9 @@ export class AppComponent {
   title = 'episjobadmin';
   orient: boolean | undefined
 
+  ngOnInit(){
+    firebase.default.initializeApp(firebaseConfig)
+  }
 
   @HostListener('window:resize', ['$event'])
   onResize() {
--- src/app/comp/rigs/rigs.component.ts
+++ src/app/comp/rigs/rigs.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { BackService } from '../../serv/back.service'
+import { from } from 'rxjs'
+import * as firebase from 'firebase/app'
+import 'firebase/database'
 
 @Component({
   selector: 'episjob-rigs',
@@ -8,14 +11,27 @@ import { BackService } from '../../serv/back.service'
   styleUrls: ['./rigs.component.scss']
 })
 export class RigsComponent implements OnInit {
-
+  rigs:any;
+  t:any;
   constructor(public router: Router, public bak:BackService) { }
 
   ngOnInit(): void {
+    this.t= Math.floor(window.innerWidth/300)
+    console.log(this.t)
+    firebase.default.database().ref('MOL').once('value')
+    .then(snap=>{
+      this.rigs=Object.values(snap.val())
+    })
+    
   }
 
   back(){
     this.bak.backP()
+  }
+
+  resize(){
+    this.t= Math.floor(window.innerWidth/300)
+    return this.t  
   }
 
 }
