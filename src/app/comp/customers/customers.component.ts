cbdf31d588f2f4905fdc184286bc02d3ae9ad303
marcoa5 <58443501+marcoa5@users.noreply.github.com>
2021-06-18 15:31:36 +0200

    k

--- src/app/comp/customers/customers.component.ts
+++ src/app/comp/customers/customers.component.ts
@@ -11,31 +11,51 @@ import 'firebase/database'
 })
 export class CustomersComponent implements OnInit {
   filtro:any=''
-  customers:any;
+  customers:any[]=[];
   pos:string='';
   ind:number=0
+  custSales:string[]=[]
+  rigSn:string[]=[]
   constructor(public router: Router, public bak:BackService) { }
 
   ngOnInit() {
     firebase.auth().onAuthStateChanged(a=>{
       firebase.database().ref('Users/' + a?.uid).once('value',b=>{
         this.pos=b.val().Pos
-        this.ind=b.val().Area.toString()
-        console.log(this.ind)
+        this.ind=b.val().Area?.toString()
       })
       .then(()=>{
         if(this.pos!='sales'){
           firebase.database().ref('Customers').on('value', g=>{
             this.customers = Object.values(g.val())
+            
           })
-        } else {
-          firebase.database().ref('RigAuth').orderByChild('a' + this.ind).equalTo('1').once('value',u=>{
-            console.log(Object.keys(u.val()))
+        } /*else {
+          firebase.database().ref('Customers/').once('value',kj=>{
+            this.custSales = kj.val()
+            //console.log(this.custSales)
           })
-        }
+          .then(()=>{
+            firebase.database().ref('RigAuth').orderByChild('a' + this.ind).equalTo('1').once('value',u=>{
+              u.forEach(g=>{
+                firebase.database().ref('MOL/' + g.key).child('customer').once('value',hg=>{
+                  let fre = hg.val().replace('/00','')
+                  firebase.database().ref('Customers/').child(fre.replace(/\./g,'')).once('value',tr=>{
+                    let f:any = (Object.values(tr.val())[0])
+                    let s = f.toString()
+                    console.log(s)
+                    if(tr.val()!=null && !JSON.stringify(this.customers).includes(s)) {
+                      this.customers.push(tr.val())
+                    }
+                  })
+                  .catch(()=>{})
+                })
+              })
+            })
+          })
+        }*/
       })
     })
-    
   }
 
   open(a: String, b:string, c:string){
