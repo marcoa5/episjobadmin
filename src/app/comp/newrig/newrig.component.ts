264b072c1d826243705d4b68467a9ce95288d9f7
marcoa5 <marco.arato@gmail.com>
2021-09-24 10:33:55 +0200

    k

--- src/app/comp/newrig/newrig.component.ts
+++ src/app/comp/newrig/newrig.component.ts
@@ -106,7 +106,7 @@ export class NewrigComponent implements OnInit {
         sn: g[0].toUpperCase()
       })
       firebase.database().ref('RigAuth/' + g[0].toUpperCase()).set({
-        a1:0,a2:0,a3:0,a4:0,sn:g[0].toUpperCase()
+        a1:'0',a2:'0',a3:'0',a4:'0',a5:'0',sn:g[0].toUpperCase()
       })
       this.childAdd['sn']=g[0].toUpperCase()
       firebase.database().ref('Categ/'+ g[0].toUpperCase()).set(this.childAdd)
