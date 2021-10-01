6f1c1bac64b243709d275663dabc9cb000e12c6a
marcoa5 <marco.arato@gmail.com>
2021-10-01 11:32:06 +0200

    k

--- src/app/comp/home/home.component.ts
+++ src/app/comp/home/home.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/database'
+import * as moment from 'moment';
 
 @Component({
   selector: 'episjob-home',
@@ -11,7 +12,7 @@ import 'firebase/database'
 })
 export class HomeComponent implements OnInit {
   pos:string=''
-  nome:string|null=''
+  nome:string=''
   spin:boolean=true
   buttons:any = [
     {
@@ -93,9 +94,9 @@ export class HomeComponent implements OnInit {
       console.log('Received foreground message ', p)
     })
     firebase.auth().onAuthStateChanged(a=>{
-      if(a!=null) this.nome = a.email
-      firebase.database().ref('Users/'+a?.uid).child('Pos').once('value',b=>{
-        this.pos=b.val()
+      firebase.database().ref('Users/'+a?.uid).once('value',b=>{
+        this.pos=b.val().Pos
+        this.nome=b.val().Nome + ' ' + b.val().Cognome
       })
       .then(()=>{
         messaging.getToken({vapidKey:'BETaY1oMq6ONzg-9B-uNHl27r4hcKd5UVH-EgNEXLQ9kUzqDwGq8nZwZTDN0klxbC-Oz-nSz6yGTzDD0R4h_vXY'})
@@ -103,7 +104,8 @@ export class HomeComponent implements OnInit {
           firebase.database().ref('Tokens').child(t).set({
             token: t,
             pos: this.pos,
-            name: this.nome
+            name: this.nome,
+            date: moment(new Date()).format('YYYY-MM-DD'),
           })
         })
         this.spin=false
