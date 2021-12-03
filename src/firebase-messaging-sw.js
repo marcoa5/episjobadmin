5e996c20f0088e6e23461e06236c8f3a950fa6fc
marcoa5 <marco.arato@gmail.com>
2021-12-03 10:58:44 +0100

    k

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -11,6 +11,7 @@ firebase.initializeApp({
     appId: "1:793133030101:web:1c046e5fcb02b42353a05c",
     measurementId: "G-Y0638WJK1X"
   });
+  const messaging = firebase.messaging()
   var url=''
   self.addEventListener('notificationclick', function(event) {
     event.notification.close()
@@ -23,21 +24,34 @@ firebase.initializeApp({
   })
 
   if (firebase.messaging.isSupported()){
-    const messaging = firebase.messaging()
-    
     messaging.onBackgroundMessage((payload) => {
-      url=payload.data.url
-      console.log('Received background message ', payload.data.title);
-      const notificationTitle = payload.data.title;
-      const notificationOptions = {
-        body: payload.data.body,
-        badge: 'https://raw.githubusercontent.com/marcoa5/episjobadmin/master/src/assets/icons/logo.png',
-        icon: 'https://raw.githubusercontent.com/marcoa5/episjobadmin/master/src/assets/icons/logo.png',
-        //tag: payload.data.title.substring(0,9)=='New Visit'? 'visit' : 'sj',
-        //requireInteraction: true
-      };      
+      if(payload.data.type=='visit'){
+        let p = JSON.parse(payload.data.info)
+        url = 'visit;day=' + p.date
+        const notificationTitle = 'New visit by ' + p.sam;
+        const notificationOptions = {
+          body: p.c1,
+          badge: 'https://raw.githubusercontent.com/marcoa5/episjobadmin/master/src/assets/icons/logo.png',
+          icon: 'https://raw.githubusercontent.com/marcoa5/episjobadmin/master/src/assets/icons/logo.png',
+          //tag: payload.data.title.substring(0,9)=='New Visit'? 'visit' : 'sj',
+          requireInteraction: true
+        };      
+      self.registration.showNotification(notificationTitle, notificationOptions)
+      }
+
+      if(payload.data.type=='sj'){
+        url='files'
+        const notificationTitle = 'New Service Job';
+        const notificationOptions = {
+        body: payload.data.info,
+          badge: 'https://raw.githubusercontent.com/marcoa5/episjobadmin/master/src/assets/icons/logo.png',
+          icon: 'https://raw.githubusercontent.com/marcoa5/episjobadmin/master/src/assets/icons/logo.png',
+          //tag: payload.data.title.substring(0,9)=='New Visit'? 'visit' : 'sj',
+          requireInteraction: true
+        };      
+      return self.registration.showNotification(notificationTitle, notificationOptions)
+      }
       
-      return self.registration.showNotification(notificationTitle, notificationOptions);
     })
 
     
