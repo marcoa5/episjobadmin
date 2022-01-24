f555108fc41da0d17cb7115bbac221141b68e986
marcoa5 <marco.arato@gmail.com>
2022-01-24 17:09:26 +0100

    k

--- src/app/comp/sj/sj.component.ts
+++ src/app/comp/sj/sj.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as moment from 'moment';
 import { Subscription } from 'rxjs';
 import { AuthServiceService } from 'src/app/serv/auth-service.service';
 
@@ -18,7 +19,9 @@ export class SjComponent implements OnInit {
   tech:any[]=[]
   rigForm!:FormGroup
   searchForm!:FormGroup
-  custForm!:FormGroup
+  reportForm!:FormGroup
+  signatureForm!:FormGroup
+  hoursForm!:FormGroup
   subsList:Subscription[]=[]
   sn:string=''
   spin:boolean=true
@@ -27,13 +30,28 @@ export class SjComponent implements OnInit {
       search:''
     })
     this.rigForm=fb.group({
+      date: [moment(new Date()).format('YYYY-MM-DD')],
       sn: ['', Validators.required],
+      pn: [''],
       model: ['', Validators.required],
       customer: ['', Validators.required],
+      customer2: [''],
+      customer3: [''],
       site: ['', Validators.required],
+      engh: [''],
+      perc1h: [''],
+      perc2h: [''],
+      perc3h: [''],
     })
-    this.custForm=this.fb.group({
-      search:''
+    this.reportForm=this.fb.group({
+      report:['', Validators.required],
+      oss:['']
+    }),
+    this.hoursForm=fb.group({
+
+    })
+    this.signatureForm=fb.group({
+      
     })
   }
 
@@ -71,6 +89,7 @@ export class SjComponent implements OnInit {
 
   searchCust(e:any){
     let a = e.target.value
+    console.log(a, a.length)
     this.rigs=this._rigs.filter((b:any)=>{
       return(b.sn.toLowerCase().includes(a) || b.model.toLowerCase().includes(a) || b.customer.toLowerCase().includes(a) || b.site.toLowerCase().includes(a) )
     })
@@ -89,10 +108,16 @@ export class SjComponent implements OnInit {
       this.rigForm.controls.sn.setValue(this.rigs[0].sn)
       this.rigForm.controls.model.setValue(this.rigs[0].model)
       this.rigForm.controls.customer.setValue(this.rigs[0].customer)
+      this.rigForm.controls.customer2.setValue(this.customers[this.rigs[0].custid].c2)
+      this.rigForm.controls.customer3.setValue(this.customers[this.rigs[0].custid].c3)
       this.rigForm.controls.site.setValue(this.rigs[0].site)
-      console.log(this.rigs[0].custid)
     } else {
       this.rigForm.reset()
     }
+  }
+
+  calc(a:number){
+    if(a==1) return (Math.round(1/window.innerWidth*10000)+3)
+    return (Math.round(1/window.innerWidth*10000)+3)/2
   }
 }
