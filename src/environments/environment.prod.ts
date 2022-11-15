7c799d2c5d2ce768d45150fa8d80e405e1a55220
marcoa5 <marco.arato@gmail.com>
2022-11-15 16:57:49 +0100

    k

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -28,7 +28,7 @@ export const environment = {
 	],
   appVersion: require('../../package.json').version,
   production: true,
-  //url: 'http://localhost:3001/'
-  url: 'https://episjobreq.herokuapp.com/'
+  //url: 'http://localhost:3001/iyc/'
+  url: 'https://episjobreq.herokuapp.com/iyc/'
   //url: '/api/'
 };
